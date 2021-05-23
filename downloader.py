d93cb0b2af32c5fdaca5143179c1d7f576e1e5fb
PilotProgrammer <17863859+PilotProgrammer@users.noreply.github.com>
2021-05-23 16:52:54 -0400

    multiple chapters

--- downloader.py
+++ downloader.py
@@ -10,19 +10,19 @@ import PyPDF2
 # 3. get list of chapters automatically?
 
 chapters = {
-    '1317': range(1, 20)
+    '1317': range(1, 50),
+    '1318': range(1, 50),
+    '1319': range(1, 50),
+    '1320': range(1, 50),
+    '1321': range(1, 50),
+    '1322': range(1, 50)
 }
 
-headers = {"Cookie": "<<your creds here>>"}
-
+headers = {"Cookie": "ASP.NET_SessionId=<<CRED>>; Username=<<CRED>>; ASPSESSIONIDSCVTDSRQ=<<CRED>>;"}
 
 chapterNames = list(chapters.keys())
 
-htmlFileNames = []
-chapterPdfFileNames = []
-
-
-def combinePdfs(chapterPdfFileNames):
+def combinePdfs(chapterPdfFileNames, chapterName):
 	pdfWriter = PyPDF2.PdfFileWriter()
 
 	for filename in chapterPdfFileNames:
@@ -33,52 +33,61 @@ def combinePdfs(chapterPdfFileNames):
 			pageObj = pdfReader.getPage(pageNum)
 			pdfWriter.addPage(pageObj)
 
-	pdfOutput = open('out.pdf', 'wb')
+	pdfOutput = open(chapterName + '.pdf', 'wb')
 	pdfWriter.write(pdfOutput)
 	pdfOutput.close()
+
+def sanitizeForm(theForm):
+		# remove executable code and links to content which may not be reachable from script (just keep content text and images)
+	for element in theForm.find_all('script'):
+			element.decompose()
+
+	for element in theForm.find_all('style'):
+			element.decompose()
+
+	for element in theForm.find_all('a'):
+			element.decompose()
+
+	for element in theForm.find_all('iframe'):
+			element.decompose()
+
+	# remove navigation divs, with dead links to navigation icons
+	for element in theForm.find_all('div', {'class': 'NavControl'}):
+			element.decompose()
+
+	return theForm
+
+def isLastPageOfChapter(theForm):
+	hasSection = False
+	for element in theForm.find_all('div', {'class': 'Section'}):
+		if element is not None:
+			hasSection = True
+			break
+
+	return not hasSection
 
 
 for chapterName in chapterNames:
 	pageNums = chapters[chapterName]
+	chapterPdfFileNames = []
 
 	for pageNum in pageNums:
-		print(f'pageNum {pageNum}')
+		print(f'Chapter {chapterName} page number {pageNum}')
 		URL = f"https://americanflyers.net/content/browser/chapter.aspx?CID={chapterName}&Page={pageNum}"
-		htmlFileName = f'firc-{chapterName}-{pageNum}.html'
-		chapterPdfFileName = f'firc-{chapterName}-{pageNum}.pdf'
-		htmlFileNames.append(htmlFileName)
-		chapterPdfFileNames.append(chapterPdfFileName)
 
 		resp = requests.get(url=URL, headers=headers)  # , params = PARAMS)
 		soup = BeautifulSoup(resp.text, features="html.parser")
 		theForm = soup.find(id='aspnetForm')
 
-		# remove executable code and links to content which may not be reachable from script (just keep content text and images)
-		for element in theForm.find_all('script'):
-				element.decompose()
+		theForm = sanitizeForm(theForm)
 
-		for element in theForm.find_all('style'):
-				element.decompose()
-
-		for element in theForm.find_all('a'):
-				element.decompose()
-
-		for element in theForm.find_all('iframe'):
-				element.decompose()
-
-		# remove navigation divs, with dead links to navigation icons
-		for element in theForm.find_all('div', {'class': 'NavControl'}):
-				element.decompose()
-
-		hasSection = False
-		for element in theForm.find_all('div', {'class': 'Section'}):
-			if element is not None:
-				hasSection = True
-				break
-
-		if hasSection == False:
-			print (f'No Section, must be last page, ending...')
+		if isLastPageOfChapter(theForm) == True:
+			print (f'No Section, must be last page of chapter...')
 			break
+		
+		htmlFileName = f'firc-{chapterName}-{pageNum}.html'
+		chapterPdfFileName = f'firc-{chapterName}-{pageNum}.pdf'
+		chapterPdfFileNames.append(chapterPdfFileName)
 
 		with open(htmlFileName, 'w') as file:  # Use file to refer to the file object
 				file.write(str(theForm))
@@ -86,10 +95,7 @@ for chapterName in chapterNames:
 		pdfkit.from_file(htmlFileName, chapterPdfFileName)
 		os.remove(htmlFileName)
 
-		# if pageNum > 3:
-		# 	break
-
-	combinePdfs(chapterPdfFileNames)
+	combinePdfs(chapterPdfFileNames, chapterName)
 
 	for chapterPdfFileName in chapterPdfFileNames:
 		os.remove(chapterPdfFileName)
